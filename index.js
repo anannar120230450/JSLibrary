821b39200829119c3d21cbdfb8f6f94d6fa091b8
anannar120230450 <anannar120230450>
2024-06-08 01:07:17 +0100

    cretae base functions

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
+/* book object:
+{
+    read: BOOL,
+    title: STRING,
+    author: STRING,
+    pageCount: INT
+}
+*/
 const library = [];
 const htmlElements = {
     contentBooksView: document.querySelector(".content-books-view"),
 };
+
+function Book(title, author, pageNum, read) {
+    this.title = title;
+    this.author = author;
+    this.pageNum = pageNum;
+    this.read = read;
+};
+
+Book.prototype.toggleRead = () => {
+    this.read = !this.read;
+};
+
+function addToLib(title, author, pageNum, read) {
+    library.push(new Book(title, author, pageNum, read));
+}
