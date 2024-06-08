763b8e642c8f45bd5c41a6df6fd269bf2ec44c76
anannar120230450 <anannar120230450>
2024-06-08 02:45:32 +0100

    cretae form for adding new books

--- index.js
+++ index.js
@@ -10,13 +10,29 @@ const library = [];
 const htmlElements = {
     addNewBookBtn: document.getElementById("add-new-book-btn"),
     contentBooksView: document.querySelector(".content-books-view"),
+    addNewBookFormBackdrop: document.querySelector(".add-new-book-form-backdrop"),
+    addNewBookFormTitleInput: document.querySelector("#add-new-book-form-title-input"),
+    addNewBookFormAuthorInput: document.querySelector("#add-new-book-form-author-input"),
+    addNewBookFormPagesInput: document.querySelector("#add-new-book-form-pages-input"),
+    addNewBookFormAddBtn: document.querySelector("#add-new-book-form-add-btn"),
 };
 
 window.addEventListener("DOMContentLoaded", () => {
-    ////////// add new project btn event ///////////
+    // initial book examples
+    addToLib("Crime And Punishment", "Fyodor Dostoevsky", "527", true);
+    renderLib();
+
+    // add new book header button
     htmlElements.addNewBookBtn.addEventListener("click", () => {
-        addToLib("Ever", "hhas", "235", true);
+        htmlElements.addNewBookFormBackdrop.style.display = "flex";
+        /*addToLib("Ever", "hhas", "235", true);
+        renderLib();*/
+    });
+    // add new book form button
+    htmlElements.addNewBookFormAddBtn.addEventListener("click", () => {
+        addToLib(htmlElements.addNewBookFormTitleInput.value, htmlElements.addNewBookFormAuthorInput.value, htmlElements.addNewBookFormPagesInput.value, true);
         renderLib();
+        htmlElements.addNewBookFormBackdrop.style.display = "none";
     });
 });
 
@@ -47,7 +63,7 @@ function renderLib() {
             </div>
             <div class="book-card-info">
                 <p class="book-card-info-author">${book.author}</p>
-                <p class="book-card-info-pages">${book.pageNum}</p>
+                <p class="book-card-info-pages">${book.pageNum} Pages</p>
             </div>
             <div class="book-card-tooling">
                 ${function() {
